6fc8172c0e3e0338b96b913323158d885d56e16d
tmcarron <tmcarron7@gmail.com>
2021-01-22 10:13:06 -0500

    Searching for vowels in a word challenge from Edabit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word.");
+            string word = Console.ReadLine();
+            Console.WriteLine("There are " + CountVowels(word) + " vowels in " + word);
+        }
+        static int CountVowels(string word)
+        {
+            int vowelcount = 0;
+            string vowels = "aeiou";
+            for(int i = 0; i < word.Length; i++)
+            {
+                for(int j = 0; j < vowels.Length; j++)
+                {
+                    if(word[i] == vowels[j])
+                    {
+                        vowelcount += 1;
+                        break;
+                    }
+                }
+            }
+            return vowelcount;
+        }
+
+    }
+}
